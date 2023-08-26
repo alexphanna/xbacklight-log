c53864630a82d6a637c184b72011bab83696f43b
alexphanna <ahanna0608@gmail.comgit config --global user.name alexphannagit config --global user.email ahanna0608@gmail.com>
2023-08-25 23:31:30 -0400

    pickled

--- brightness.py
+++ brightness.py
@@ -1,14 +1,15 @@
-from math import log10
+import math
 import pickle
 import subprocess
 import sys
 
 def get_measured():
     return int(subprocess.run(['xbacklight', '-get'], capture_output=True, text=True).stdout)
+
 def get_perceived():
     with open('/home/alex/Brightness/perceived_brightness', 'rb') as fi:
         return pickle.load(fi)
-
+        
 def set_brightness(perceived):
     with open('/home/alex/Brightness/perceived_brightness', 'wb') as fi:
         pickle.dump(perceived, fi)
@@ -16,10 +17,10 @@ def set_brightness(perceived):
 
 def perceived_to_measured(perceived):
     if perceived == 0: return 0
-    else: return int(10**((perceived / 100) * 2 - 2) * 100)
+    else: return round(10**((perceived / 100) * 2 - 2) * 100)
 
 def measured_to_perceived(measured):
-    return int((log10(measured / 100) + 2) / 2 * 100)
+    return round((math.log10(measured / 100) + 2) / 2 * 100)
 
 if (len(sys.argv) == 2):
     if (sys.argv[1] == "-inc" and get_perceived() < 100): set_brightness(get_perceived() + 5)
