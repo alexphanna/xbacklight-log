f67329e520abac107bdab6c91cfdac6e39429d97
alexphanna <ahanna0608@gmail.comgit config --global user.name alexphannagit config --global user.email ahanna0608@gmail.com>
2023-08-28 14:08:33 -0400

    Bug fixes

--- brightness.py
+++ brightness.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 import math
+import os
 import pickle
 import subprocess
 import sys
@@ -9,11 +10,11 @@ def get_measured():
     return int(subprocess.run(['xbacklight', '-get'], capture_output=True, text=True).stdout)
 
 def get_perceived():
-    with open('/home/alex/Brightness/perceived_brightness', 'rb') as fi:
+    with open(os.path.dirname(os.path.realpath(__file__)) + '/perceived_brightness', 'rb') as fi:
         return pickle.load(fi)
         
 def set_brightness(perceived):
-    with open('/home/alex/Brightness/perceived_brightness', 'wb') as fi:
+    with open(os.path.dirname(os.path.realpath(__file__)) + '/perceived_brightness', 'wb') as fi:
         pickle.dump(perceived, fi)
     subprocess.run(['xbacklight', '-set', str(perceived_to_measured(perceived))])
 
@@ -24,9 +25,12 @@ def perceived_to_measured(perceived):
 def measured_to_perceived(measured):
     return round((math.log10(measured / 100) + 2) / 2 * 100)
 
-if (len(sys.argv) != 2):
-    print("usage: brightness.py [-get] [-inc] [-dec]\nbrightness.py: error: one of the arguments -get -inc -dec is required")
-else:
-    if (sys.argv[1] == "-inc" and get_perceived() < 100): set_brightness(get_perceived() + 5)
-    elif (sys.argv[1] == "-dec" and get_perceived() > 0): set_brightness(get_perceived() - 5)
-    elif (sys.argv[1] == "-get"): print(get_perceived())
+if __name__ == '__main__':
+    if not os.path.exists(os.path.dirname(os.path.realpath(__file__)) + '/perceived_brightness'):
+        set_brightness(100)
+    if (len(sys.argv) != 2):
+        print("usage: brightness.py [-get] [-inc] [-dec]\nbrightness.py: error: one of the arguments -get -inc -dec is required")
+    else:
+        if (sys.argv[1] == "-inc" and get_perceived() < 100): set_brightness(get_perceived() + 5)
+        elif (sys.argv[1] == "-dec" and get_perceived() > 0): set_brightness(get_perceived() - 5)
+        elif (sys.argv[1] == "-get"): print(get_perceived())
